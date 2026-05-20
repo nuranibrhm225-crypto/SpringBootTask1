e2e36576def0997944143e6bf97b9a02ccd1d27e
nuranibrhm225-crypto <nuranibrhm225@gmail.com>
2026-05-20 22:17:42 +0200

    Task 1 fixed

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
+    @ResponseBody
     @GetMapping("/")
-    public String greeting() {
+    public String hello() {
+        return "Hello Vistula, in my first Spring controller.";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
         return "greeting";
     }
 }
